0c2fda13b0c8d522dbea45859bb50adb8d43db13
Vitaly Zdanevich <vitaly.zdanevich@waveoc.com>
2018-08-04 13:11:24 +0300

    Badge on button after filtering

--- eventPage.js
+++ eventPage.js
@@ -2,4 +2,7 @@ chrome.browserAction.onClicked.addListener(_ => {
     chrome.tabs.executeScript({
         'file': 'contentScript.js'
     });
+
+    chrome.browserAction.setBadgeBackgroundColor({'color': 'green'});
+    chrome.browserAction.setBadgeText({'text': 'ok'});
 });
