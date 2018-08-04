1aa4b0bd1286f65f80e158a17a1517daf3bc5034
Vitaly Zdanevich <vitaly.zdanevich@waveoc.com>
2018-08-04 13:03:57 +0300

    MVP done
    Still without filtering of the new appearing tutors from AJAX

--- contentScript.js
+++ contentScript.js
@@ -1,7 +1,16 @@
 if (location.host === 'www.cambly.com') {
+
+    const mins = document.querySelector('#student-plan-info h4').innerText.replace(/\D+/, '');
+
     for (const card of document.getElementsByClassName('tutor-card-mui')) {
 
-        // I tried node.remove() and faced with broken page
-        card.style.display = 'none';
+        const nodeBusy = card.querySelector('small');
+
+        if (!nodeBusy) continue;
+
+        if (nodeBusy.innerText.replace(/\D+/, '') < mins)
+            // I tried node.remove() and faced with broken page
+            card.style.display = 'none';
     }
+
 }
